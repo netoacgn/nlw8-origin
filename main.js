fa0cd9541478fc83e4f635ccb0dfdee9b4cee02a
Antonio Neto <acgomesn@gmail.com>
2022-05-06 17:25:10 -0300

    Atualizações do Stage3

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-function onScroll(){
-    
-    if(scrollY > 0){
+function onScroll() {
+
+    if (scrollY > 0) {
         navigation.classList.add('scroll')
-    }else {
+    } else {
         navigation.classList.remove('scroll')
     }
 }
 
-function openMenu(){
+function openMenu() {
     document.body.classList.add('menu-expanded')
 }
 
-function closeMenu(){
+function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+}).reveal(`
+    #home, 
+    #home img, 
+    #home .stats, 
+    #services,
+    #services header,
+    #services .card,
+    #about,
+    #about header,
+    #about content
+    ]
+    `);
